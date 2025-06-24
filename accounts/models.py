28158c9bcdb95965300dc75534a163b0cb345ff9
Pumulo Mufalali <crispumulo@gmail.com>
2025-06-24 02:14:13 +0200

    create user

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
+  user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
   name = models.CharField(max_length=200, null=True)
   phone = models.CharField(max_length=200, null=True)
+  profile_pic = models.ImageField(null=True, blank=True)
   email = models.CharField(max_length=200, null=True, unique=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
