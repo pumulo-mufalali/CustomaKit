97cb79a12cb651e9de0029fd76892d9f7053d4a8
Pumulo Mufalali <crispumulo@gmail.com>
2025-06-24 02:29:43 +0200

    create user added

--- crm/settings.py
+++ crm/settings.py
@@ -128,3 +128,6 @@ STATICFILE_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'accounts/static/images')
